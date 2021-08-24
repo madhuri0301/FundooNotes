2564d06ef7ff946b1fc21ed1ef09230258eee44d
Madhuri <harshupatil0301@gmail>
2021-08-24 08:04:23 +0530

    [Madhuri]Refactor : Creating A Forget Password Page

--- src/app/Components/forget-password/forget-password.component.ts
+++ src/app/Components/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent implements OnInit {
+  forgetpassword!: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.forgetpassword = this.formBuilder.group({
 
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      service: ['advance', Validators.requiredTrue]
+    });
+   }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+   get f() { return this.forgetpassword.controls; }
 
-}
+   onSubmit() {
+     this.submitted = true;
+ 
+     // stop here if form is invalid
+     if (this.forgetpassword.invalid) {
+       return;
+     }
+ 
+     // display form values on success
+     alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.forgetpassword.value, null, 4));
+   }
+ 
+ }
+ 
